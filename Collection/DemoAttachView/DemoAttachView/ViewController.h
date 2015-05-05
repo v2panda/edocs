6cec31b51bf8a0ee3fe4b210a6eb7fec0a9f7e45
xuzhen2014 <302751643@qq.com>
2015-05-05 11:31:39 +0800

    add Attach

---
+++ Collection/DemoAttachView/DemoAttachView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoAttachView
+//
+//  Created by 徐臻 on 15/2/10.
+//  Copyright (c) 2015年 xuzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
