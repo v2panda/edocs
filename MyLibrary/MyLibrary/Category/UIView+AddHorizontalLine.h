6dcdc1886f6ae98d79f8fbb65ebc2f095ebda844
panda <pdxuzhen@gmail.com>
2015-09-24 11:05:13 +0800

    add View Category

---
+++ MyLibrary/MyLibrary/Category/UIView+AddHorizontalLine.h
@@ -0,0 +1,16 @@
+//
+//  UIView+AddHorizontalLine.h
+//  Meituan
+//
+//  Created by 臧其龙 on 15/9/18.
+//  Copyright (c) 2015年 臧其龙. All rights reserved.
+//
+//  给View中间加横线 例 a̶b̶c̶d̶
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (AddHorizontalLine)
+
+- (void)addHorizontalLineWithColor:(UIColor *)lineColor;
+
+@end
