34bdbc688208757786a7c34329ff161b661109f6
v2panda <pdxuzhen@gmail.com>
2015-10-20 18:00:51 +0800

    ScrollingNavbar

---
+++ StudyDemo/ScrollingNavbar/PDAScrollingNavbar/PDAScrollingNavbar/PDATableViewController.m
@@ -0,0 +1,99 @@
+//
+//  PDATableViewController.m
+//  Demo-0907SB
+//
+//  Created by Panda on 15/9/10.
+//  Copyright (c) 2015年 徐臻. All rights reserved.
+//
+
+#import "PDATableViewController.h"
+#import "AMScrollingNavbar.h"
+@interface PDATableViewController ()
+
+@end
+
+@implementation PDATableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self followScrollView:self.tableView withDelay:20.0];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELLID" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text = [NSString stringWithFormat:@"第几%ld行",indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"IMG_0007"];
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ StudyDemo/ScrollingNavbar/PDAScrollingNavbar/PDAScrollingNavbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDAScrollingNavbar
+//
+//  Created by Panda on 15/10/20.
+//  Copyright © 2015年 Panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
