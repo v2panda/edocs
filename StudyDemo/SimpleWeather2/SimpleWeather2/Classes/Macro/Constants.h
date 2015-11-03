83be18edcab21cf13cdee9d8bde5e161467f141f
wuzhe <1123@forex.com.cn>
2015-11-03 14:07:28 +0800

    add SimpleWeather2

---
+++ StudyDemo/SimpleWeather2/SimpleWeather2/Classes/Macro/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  SimpleWeather2
+//
+//  Created by Panda on 15/10/29.
+//  Copyright © 2015年 Panda. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+
+#endif /* Constants_h */
---
+++ StudyDemo/SimpleWeather2/SimpleWeather2/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeather2
+//
+//  Created by Panda on 15/10/29.
+//  Copyright © 2015年 Panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
